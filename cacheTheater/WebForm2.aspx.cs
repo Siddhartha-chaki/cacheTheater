f0731d9e132265d483509cd151315f3a345be39d
Administrator <Administrator@HCL-PC23>
2017-09-27 11:03:57 +0530

    caheced data first step

--- cacheTheater/WebForm2.aspx.cs
+++ cacheTheater/WebForm2.aspx.cs
@@ -13,20 +13,28 @@ namespace cacheTheater
         {
             try
             {
-                
                     if (Session["theater"] == null) { Response.Redirect("WebForm1.aspx"); }
                         string s=Session["theater"].ToString();
+                Label1.Text = s;
                     switch (s)
                     {
-                    case "":
+                    case "Cinemax":
                         if (Cache["Cinemax_shows"] == null)
                         {
-                            Cache["Cinemax_shows"] = "Judwaa 2    9:00am<br>";
+                            Cache["Cinemax_shows"] = "<br>Judwaa 2    9:00am<br>";
                             
                         }
                         Label2.Text = Cache["Cinemax_shows"].ToString();
                             break;
-                        default:
+                    case "PVR Phoenix Market City":
+                        if (Cache["pvr_shows"] == null)
+                        {
+                            Cache["pvr_shows"] = "<br>Newton    9:00am";
+
+                        }
+                        Label2.Text = Cache["pvr_shows"].ToString();
+                        break;
+                    default:
                             break;
                     }
                 }
